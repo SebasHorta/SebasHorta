533ab7fde03be074146c6a3ed6778ef13a39fbde
Sebas Horta <111324497+SebasHorta@users.noreply.github.com>
2023-10-06 13:38:28 -0400

    Update pdfmerger.py

--- pdfmerger.py
+++ pdfmerger.py
@@ -2,17 +2,15 @@ import PyPDF2
 import os
 
 merger = PyPDF2.PdfMerger()
-output_filename = "MergedDocs.pdf"  # Initialize the output filename
+output_filename = "MergedDocs.pdf"
 
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         print(file)
         merger.append(file)
 
-# Construct a new filename based on the input filenames
 input_pdf_files = [file for file in os.listdir(os.curdir) if file.endswith(".pdf")]
 if input_pdf_files:
-    # Extract the base filenames (without extensions) and join them with underscores
     merged_filename = "_".join(os.path.splitext(file)[0] for file in input_pdf_files) + ".pdf"
     output_filename = merged_filename
 
