2f31523022ce98a326209eda8a44cf5da0f4fe61
Sebas Horta <111324497+SebasHorta@users.noreply.github.com>
2023-10-06 13:41:04 -0400

    Add files via upload

---
+++ pdfmerger.py
@@ -0,0 +1,20 @@
+import PyPDF2
+import os
+
+merger = PyPDF2.PdfMerger()
+output_filename = "MergedDocs.pdf" 
+
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        print(file)
+        merger.append(file)
+
+input_pdf_files = [file for file in os.listdir(os.curdir) if file.endswith(".pdf")]
+if input_pdf_files:
+    merged_filename = "_".join(os.path.splitext(file)[0] for file in input_pdf_files) + ".pdf"
+    output_filename = merged_filename
+
+merger.write(output_filename)
+merger.close()
+
+print(f"Merged PDF saved as '{output_filename}'")
